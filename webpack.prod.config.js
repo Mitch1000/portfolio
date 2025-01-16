f03c29a76d949d01b2afdb1b33dada04ed413d6a
Dirt <dirt@example.com>
2025-01-16 13:55:47 -0500

    updates webpack.prod.config to properly deploy app

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,8 +1,54 @@
 const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'production',
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+      {
+        // vue-loader config to load `.vue` files or single file components.
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    // make sure to include the plugin for the magic
+    new VueLoaderPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public/assets'),
+          to: path.resolve(__dirname, 'dist/assets'),
+        },
+      ],
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
