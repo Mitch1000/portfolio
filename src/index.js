9d3e89fb5a0d38790e9332104819adecb5d35a4f
Mitch10001 <mwdrohan@gmail.com>
2025-05-14 10:17:51 -0400

    prevents race conditions when loading three and react from cdn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
+let hasSetup = false;
+
 window.setup = () => {
+  hasSetup = true;
   import('./entry_points/code');
 }
 
+const isObj = (value) => typeof value === 'object';
+const hasLoaded = isObj(window.THREE)
+  && isObj(window.React)
+  && isObj(window.ReactDOM)
+  && isObj(window.ReactFiber);
+
+if (hasLoaded && !hasSetup) {
+  window.setup();
+}
