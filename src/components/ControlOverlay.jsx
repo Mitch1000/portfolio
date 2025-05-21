3bbffee48c9ad467cdfe772e13c4a7d294f653aa
Mitch10001 <mwdrohan@gmail.com>
2025-05-21 13:03:53 -0400

    lints control overlay file

--- src/components/ControlOverlay.jsx
+++ src/components/ControlOverlay.jsx
@@ -6,56 +6,56 @@ export default function ControlOverlay() {
   };
 
   return (
-   <div className="control-overlay">
-    <div className="sliders">
-      <div className="sliders-holder">
-        <div className="slider-container">
-          <div className="slider-info ">
-            <span className="info"> &#916;t</span>
-          </div>
-          <div
-            id="time-slider"
-            className="slider--handle"
-          >
-            <div className="slider--handle-scaler">
-              <div
-                className="slider--handle-box"
-              ></div>
+    <div className="control-overlay">
+      <div className="sliders">
+        <div className="sliders-holder">
+          <div className="slider-container">
+            <div className="slider-info ">
+              <span className="info"> &#916;t</span>
             </div>
+            <div
+              id="time-slider"
+              className="slider--handle"
+            >
+              <div className="slider--handle-scaler">
+                <div
+                  className="slider--handle-box"
+                ></div>
+              </div>
+            </div>
+            <div className="slider" />
           </div>
-          <div className="slider" />
         </div>
       </div>
-    </div>
 
-     <div
-       id="scale-checkbox"
-       className="scale-checkbox"
-     >
-       Scaled:
-       <div
-         className="checkbox-wrapper-3"
-       >
-         <input
-           id="cbx-3"
-           onChange={handleCheck}
-           type="checkbox"
-           checked
-         />
-         <label
-           htmlFor="cbx-3"
-           className="toggle"
-         >
-           <span />
-         </label>
-       </div>
-     </div>
-     <div className="scenario-select-container">
-       <select
-         id="scenario-select"
-         className="scenario-select"
-       />
-     </div>
-   </div>
+      <div
+        id="scale-checkbox"
+        className="scale-checkbox"
+      >
+        Scaled:
+        <div
+          className="checkbox-wrapper-3"
+        >
+          <input
+            id="cbx-3"
+            onChange={handleCheck}
+            type="checkbox"
+            checked
+          />
+          <label
+            htmlFor="cbx-3"
+            className="toggle"
+          >
+            <span />
+          </label>
+        </div>
+      </div>
+      <div className="scenario-select-container">
+        <select
+          id="scenario-select"
+          className="scenario-select"
+        />
+      </div>
+    </div>
   );
 }
